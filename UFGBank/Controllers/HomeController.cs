ac9762755749cc800f3325c3f775fdfef066c9b5
leonardo <84354110+lrivas3@users.noreply.github.com>
2023-11-22 20:24:36 -0600

    Arreglar Brett David

--- UFGBank/Controllers/HomeController.cs
+++ UFGBank/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Diagnostics;
+using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using UFGBank.Data;
 using UFGBank.Models;
 
 namespace UFGBank.Controllers;
@@ -8,20 +10,29 @@ namespace UFGBank.Controllers;
 [Authorize]
 public class HomeController : Controller
 {
+    private readonly UFGBankDbContext _context;
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(UFGBankDbContext context, ILogger<HomeController> logger)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        var currentId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        var user = _context.AspNetUsers.Where(i => i.Id == currentId).First();
+        return View(user);
+    }
+
+    [Authorize(Roles = "Admin")]
+    public IActionResult AdminSession()
+    {
         return View();
     }
 
-    [Authorize (Roles = "Admin")]
-    public IActionResult AdminSession()
+    public IActionResult Privacy()
     {
         return View();
     }
---
+++ UFGBank/Controllers/TransactionController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using UFGBank.Areas.Identity.Data;
+using UFGBank.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using UFGBank.Data;
+using UFGBank.Models;
+
+namespace UFGBank.Controllers;
+
+[Authorize]
+public class TransactionController : Controller
+{
+    private readonly UFGBankDbContext _context;
+
+
+    public TransactionController(UFGBankDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty] public InputModel Input { get; set; }
+
+
+    //GET: Transaction/Deposit
+    public IActionResult Deposit()
+    {
+        return View();
+    }
+
+    //POST: Transaction/Deposit
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Deposit([Bind("Amount")] Transaction transaction)
+    {
+        var currentId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        transaction.BankUserID = currentId;
+        transaction.Date = DateTime.UtcNow;
+        transaction.Type = TransactionType.Deposit;
+        var bankUser = _context.AspNetUsers.Where(i => i.Id == currentId).First();
+
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                bankUser.Balance += Input.Amount;
+                _context.Transactions.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        catch (DbUpdateException)
+        {
+            ModelState.AddModelError("", "Unable to complete the transaction. " +
+                                         "Try again, and if the problem persists " +
+                                         "see your system administrator.");
+        }
+
+        return View(transaction);
+    }
+
+    //GET: Transaction/Withdraw
+    public IActionResult Withdraw()
+    {
+        return View();
+    }
+
+    //POST: Transaction/Withdraw
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Withdraw([Bind("Amount")] Transaction transaction)
+    {
+        var currentId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+        Console.WriteLine("TransactionController.cs: currentId = " + currentId);
+        transaction.BankUserID = currentId;
+        transaction.Date = DateTime.UtcNow;
+        transaction.Type = TransactionType.Withdraw;
+        var bankUser = _context.AspNetUsers.Where(i => i.Id == currentId).First();
+
+        try
+        {
+            if (ModelState.IsValid && (bankUser.Balance - Input.Amount) > 0)
+            {
+                bankUser.Balance -= Input.Amount;
+                _context.Transactions.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+        }
+        catch (DbUpdateException)
+        {
+            ModelState.AddModelError("", "Unable to complete the transaction. " +
+                                         "Try again, and if the problem persists " +
+                                         "see your system administrator.");
+        }
+
+        return View(transaction);
+    }
+
+    //GET: Transaction/PersonalTransactions
+    public async Task<IActionResult> PersonalTransactions()
+    {
+        var currentId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+        var userTransactions = await _context.AspNetUsers
+            .Include(s => s.Transactions)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(m => m.Id == currentId);
+
+        UtcToLocalDate(userTransactions);
+
+        return View(userTransactions);
+    }
+
+    //maybe should be a Post call so id doesn't show up in URL
+    //GET: Transaction/PersonalTransactions/id
+    [Route("{id}")]
+    public async Task<IActionResult> PersonalTransactions(string id)
+    {
+        var currentId = id;
+
+        var userTransactions = await _context.AspNetUsers
+            .Include(s => s.Transactions)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(m => m.Id == currentId);
+
+        UtcToLocalDate(userTransactions);
+
+        return View(userTransactions);
+    }
+
+    public class InputModel : Transaction
+    {
+    }
+
+    public void UtcToLocalDate(UFGBankUser user)
+    {
+        DateTime utcToLocal;
+
+        foreach (var transaction in user.Transactions)
+        {
+            utcToLocal = transaction.Date.ToLocalTime();
+            transaction.Date = utcToLocal;
+        }
+    }
+}
